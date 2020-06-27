6a6a3a7a029beed14be0b227e975b2f6f46580fc
Jara Santamaria <jarasmar@gmail.com>
2020-06-27 17:44:16 +0100

    Add and style trash icon for list elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
-import ListItems from './ListItems.js'
+import ListItems from './ListItems.js';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+// Add the icons we need to the library
+library.add(faTrash);
 
 class App extends React.Component{
   constructor(props) {
