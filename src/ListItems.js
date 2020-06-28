18469925773fa902ef6622a90f4023de9f6fd501
Jara Santamaria <jarasmar@gmail.com>
2020-06-28 17:23:42 +0100

    Change list elements text into input field

--- src/ListItems.js
+++ src/ListItems.js
@@ -7,7 +7,12 @@ function ListItems(props) {
   const listItems = items.map(item =>
     {
       return <div className='list' key={item.key}>
-        <p>{item.text}
+        <p>
+          <input type='text' 
+          id={item.key} 
+          value={item.text}
+          />
+          
           <span>
             <FontAwesomeIcon className='faicons' icon='trash' 
             onClick={ () => props.deleteItem(item.key) }/>
