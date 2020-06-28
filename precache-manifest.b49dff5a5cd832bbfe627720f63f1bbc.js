4057662fee9ab6eeb1e27b3ab022de9568110bcd
Jara Santamaria <jarasmar@gmail.com>
2020-06-28 17:59:10 +0100

    Updates

---
+++ precache-manifest.b49dff5a5cd832bbfe627720f63f1bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8891b0c4a30e96ae1abc0908b5b17af0",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "d827ced878e7bab411fe",
+    "url": "/To-Do-List/static/css/main.3a38f508.chunk.css"
+  },
+  {
+    "revision": "7f8b248f50e2f94feb49",
+    "url": "/To-Do-List/static/js/2.16e61c0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.16e61c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d827ced878e7bab411fe",
+    "url": "/To-Do-List/static/js/main.5414a5d9.chunk.js"
+  },
+  {
+    "revision": "0049c03e32262d07a6e1",
+    "url": "/To-Do-List/static/js/runtime-main.35de8bd3.js"
+  }
+]);
